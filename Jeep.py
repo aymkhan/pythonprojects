c103ed7904e1dd89c77ed5ac74ae6266d5b50f7d
Aymen K <aymen@Aymens-MacBook-Air.local>
2021-09-30 23:11:12 -0700

    separated all car files and created Main.py

---
+++ Jeep.py
@@ -0,0 +1,6 @@
+from Car import *
+
+jeep1 = Car("Jeep","Wrangler",2021,"$47,000")
+jeep2 = Car("Jeep","SomeJeep",1996,"$15,900")
+
+jeep = (jeep1,jeep2)
--- Main.py
+++ Main.py
@@ -1,20 +1,7 @@
+import Honda
+import Jeep
+import Toyota
 from Car import Car
-
-toyota1 = Car("Toyota","Corolla",1998,"$8000")
-toyota2 = Car("Toyota","Camry", 2001,"$14,600")
-toyota3 = Car("Toyota","Odyssey",2020,"37,000")
-
-toyota = (toyota1,toyota2,toyota3)
-
-honda1 = Car("Honda","Civic",2012,"$23,000")
-honda2 = Car("Honda","CR-V",2015,"$34,000")
-
-honda = (honda1,honda2)
-
-jeep1 = Car("Jeep","Wrangler",2021,"$47,000")
-jeep2 = Car("Jeep","SomeJeep",1996,"$15,900")
-
-jeep = (jeep1,jeep2)
 
 print("\nWe have a small inventory of Used and New cars")
 choice = input("\nWhich make are you interested in:\nToyota, Jeep, or Honda? ")
@@ -22,7 +9,7 @@ choice2 = choice.lower()
 print("\nHere are the available cars: ")
 
 if choice2 == "toyota":
-    for x in toyota:
+    for x in Toyota.toyota:
         Car.getModelYear(x)
         print("priced at: ")
         Car.getPrice(x)
@@ -30,13 +17,13 @@ if choice2 == "toyota":
     print("You can view more cars at the dealer's website ")
     print("https://toyota.com")
     # Provide more information about the user selected car
-    car_choice = input("Which one would you like to see more info about? ")
-    for x in toyota:
-        if car_choice == str(Car.getModel(x[0])):
-            print("Corolla - good choice!")
+    #car_choice = input("Which one would you like to see more info about? ")
+    #for x in Toyota.toyota:
+    #    if car_choice == str(Car.getModel(x[0])):
+    #        print("Corolla - good choice!")
     # end of comment
 elif choice2 == "honda":
-    for x in honda:
+    for x in Honda.honda:
         Car.getModelYear(x)
         print("priced at: ")
         Car.getPrice(x)
@@ -44,7 +31,7 @@ elif choice2 == "honda":
     print("You can view more cars at the dealer's website ")
     print("https://www.honda.com")
 elif choice2 == "jeep":
-    for x in jeep:
+    for x in Jeep.jeep:
         Car.getModelYear(x)
         print("priced at: ")
         Car.getPrice(x)
